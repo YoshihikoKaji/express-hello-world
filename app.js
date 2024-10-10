88b38378dc7e6de8df3aa76a12a9f87f7af3e4d2
YoshihikoKaji <32928508+YoshihikoKaji@users.noreply.github.com>
2024-10-10 20:14:41 +0900

    変更

--- app.js
+++ app.js
@@ -1,35 +1,36 @@
-// モジュールのインポート
 const https = require("https");
 const express = require("express");
-
-// 環境変数の取得
-// ポート番号
+const app = express();
 const PORT = process.env.PORT || 3000;
-// Messaging APIを呼び出すためのトークン
 const TOKEN = process.env.LINE_ACCESS_TOKEN;
 
-// Expressアプリケーションオブジェクトの生成
-const app = express();
-
-// ミドルウェアの設定
 app.use(express.json());
-app.use(express.urlencoded({ extended: true, }));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
-// ルーティングの設定-ドメインのルート
-app.get("/", (_, res) => {
+app.get("/", (req, res) => {
   res.sendStatus(200);
 });
 
-//ルーティングの設定-MessaginAPI
-app.post("/webhook", (req, _) => {
+app.post("/webhook", function (req, res) {
+  res.send("HTTP POST request sent to the webhook URL!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
+app.post("/webhook", function (req, res) {
+  res.send("HTTP POST request sent to the webhook URL!");
+  // ユーザーがボットにメッセージを送った場合、応答メッセージを送る
   if (req.body.events[0].type === "message") {
-    res.send("HTTP POST request sent to the webhook URL!");
-    const headers = {
-      "Content-Type": "application/json",
-      Authorization: "Bearer " + TOKEN,
-    };
+    // APIサーバーに送信する応答トークンとメッセージデータを文字列化する
     const dataString = JSON.stringify({
+      // 応答トークンを定義
       replyToken: req.body.events[0].replyToken,
+      // 返信するメッセージを定義
       messages: [
         {
           type: "text",
@@ -37,32 +38,46 @@ app.post("/webhook", (req, _) => {
         },
         {
           type: "text",
-          text: "May I help you?"
+          text: "May I help you?",
         },
       ],
     });
+
+    // リクエストヘッダー。仕様についてはMessaging APIリファレンスを参照してください。
+    const headers = {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + TOKEN,
+    };
+
+    // Node.jsドキュメントのhttps.requestメソッドで定義されている仕様に従ったオプションを指定します。
     const webhookOptions = {
       hostname: "api.line.me",
       path: "/v2/bot/message/reply",
       method: "POST",
       headers: headers,
       body: dataString,
-    }
-    const request = https.request(webhookOptions, res => {
-      res.on("data", d => {
+    };
+
+    // messageタイプのHTTP POSTリクエストが/webhookエンドポイントに送信された場合、
+    // 変数webhookOptionsで定義したhttps://api.line.me/v2/bot/message/replyに対して
+    // HTTP POSTリクエストを送信します。
+
+    // リクエストの定義
+    const request = https.request(webhookOptions, (res) => {
+      res.on("data", (d) => {
         process.stdout.write(d);
       });
     });
-    request.on("error", err => {
+
+    // エラーをハンドリング
+    // request.onは、APIサーバーへのリクエスト送信時に
+    // エラーが発生した場合にコールバックされる関数です。
+    request.on("error", (err) => {
       console.error(err);
     });
 
+    // 最後に、定義したリクエストを送信
     request.write(dataString);
     request.end();
   }
-});
-
-// リスナーの設定
-app.listen(PORT, () => {
-  console.log(`Example app listening at http://localhost:${PORT}`);
 });
