15a64f43223d26094170ab8291f1972cf93fd276
YoshihikoKaji <32928508+YoshihikoKaji@users.noreply.github.com>
2024-10-10 18:32:53 +0900

    バグ修正

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const https = require("https");
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 const TOKEN = process.env.LINE_ACCESS_TOKEN;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, }));
