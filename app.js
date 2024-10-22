51f2d660b7935501eed9077bb9ca2012fdfe7db9
YoshihikoKaji <32928508+YoshihikoKaji@users.noreply.github.com>
2024-10-22 15:41:07 +0900

    fs修正

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const TOKEN = process.env.LINE_ACCESS_TOKEN;
+const fs = require('fs');
 let tempUserData = '';
 
 app.use(express.json());
