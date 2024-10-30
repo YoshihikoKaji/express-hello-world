dd22e7f3bfadd0760fd89b2144bff14e08c3be31
YoshihikoKaji <32928508+YoshihikoKaji@users.noreply.github.com>
2024-10-30 20:02:23 +0900

    変更修正

--- app.js
+++ app.js
@@ -20,7 +20,8 @@ app.get("/", (req, res) => {
 app.get("/push", (req, res) => {
   console.log("test1");
   res.send(`HTTP POST request sent to the push URL!`+ tempUserData);
-  fetch(`https://api.line.me/v2/bot/group/${tempUserData.source.groupId}/summary`)
+  const grounId = tempUserData.source.groupId;
+  fetch(`https://api.line.me/v2/bot/group/`+ grounId +`/summary`)
   .then(responce => {jsonData = response.json(); return jsonData})
   .then(responce2 => { 
     if(responce2){console.log("test2",responce2);
