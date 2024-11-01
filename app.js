89ec250a57f10800a65b1f5eeaeca622cdc70007
YoshihikoKaji <32928508+YoshihikoKaji@users.noreply.github.com>
2024-11-01 19:40:28 +0900

    etst

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const TOKEN = process.env.LINE_ACCESS_TOKEN;
 const fs = require('fs');
 // const request = require('request');
 let tempUserData = '';
+let tempGroupData = '';
 let tempUserDataOrg = '';
 
 app.use(express.json());
@@ -20,7 +21,7 @@ app.get("/", (req, res) => {
 });
 app.get("/push", (req, res) => {
   console.log("test1");
-  res.send(`HTTP POST request sent to the push URL!`+ tempUserData);
+  res.send(`HTTP POST request sent to the push URL!`+ tempUserData + tempUserData);
   const groupId = tempUserDataOrg.source.groupId;
   fetch("https://api.line.me/v2/bot/group/"+ groupId +"/summary")
   .then(responce => {jsonData = response.json(); return jsonData})
@@ -53,80 +54,15 @@ app.post("/webhook", function (req, res) {
   //     case "message":
       case "join":
         console.log("webhook");
-        // const userData = { userId: req.body.events[0].source.userId }
+        const userData = { userId: req.body.events[0].source.userId }
         // fs.writeFileSync('./user_data.json', JSON.stringify(userData));
         const groupData = { groupId: req.body.events[0].source.groupId}
         fs.writeFileSync('./user_data.json', JSON.stringify(groupData));
         tempUserDataOrg = req.body.events[0];
-        tempUserData = JSON.stringify(req.body.events[0]);
+        tempUserData = userData
+         tempGroupData = JSON.stringify(req.body.events[0]);
 }
 
-  //プッシュメッセージテストここまで
-
-  //応答メッセージテストここから
-  // ユーザーがボットにメッセージを送った場合、応答メッセージを送る
-  /*
-  if (req.body.events[0].type === "message") {
-    // APIサーバーに送信する応答トークンとメッセージデータを文字列化する
-    const dataString = JSON.stringify({
-      // 応答トークンを定義
-      replyToken: req.body.events[0].replyToken,
-      // 返信するメッセージを定義
-      messages: [
-        {
-          type: "text",
-          text: "Hello, user",
-        },
-        {
-          type: "text",
-          text: "May I help you?",
-        },
-      ],
-    });
-
-    // リクエストヘッダー。仕様についてはMessaging APIリファレンスを参照してください。
-    const headers = {
-      "Content-Type": "application/json",
-      Authorization: "Bearer " + TOKEN,
-    };
-
-    // Node.jsドキュメントのhttps.requestメソッドで定義されている仕様に従ったオプションを指定します。
-    const webhookOptions = {
-      hostname: "api.line.me",
-      path: "/v2/bot/message/reply",
-      method: "POST",
-      headers: headers,
-      body: dataString,
-    };
-
-    // messageタイプのHTTP POSTリクエストが/webhookエンドポイントに送信された場合、
-    // 変数webhookOptionsで定義したhttps://api.line.me/v2/bot/message/replyに対して
-    // HTTP POSTリクエストを送信します。
-
-    // リクエストの定義
-    const request = https.request(webhookOptions, (res) => {
-      res.on("data", (d) => {
-        process.stdout.write(d);
-      });
-    });
-
-    // エラーをハンドリング
-    // request.onは、APIサーバーへのリクエスト送信時に
-    // エラーが発生した場合にコールバックされる関数です。
-    request.on("error", (err) => {
-      console.error(err);
-    });
-
-    // 最後に、定義したリクエストを送信
-    request.write(dataString);
-    request.end();
-  }
-  */
-  //応答メッセージテストここまで
-});
-
-//プッシュメッセージの送信処理テストここから
-// function pushMessage(messages, userData) {
   function pushMessage(messages) {
   const HEADERS = {
       "Content-Type": "application/json",
@@ -168,4 +104,3 @@ app.post("/webhook", function (req, res) {
   request.end();
   console.log("test9");
 }
-//プッシュメッセージの送信処理テストここまで
